314785665875c9ef1ebcc02f1c90416ef891e99d
Gaige Lama <glama@alphatech-computing.com>
2019-09-01 23:34:17 -0400

    Add Vasri Service Provider and config
    Signed-off-by: Gaige Lama <glama@alphatech-computing.com>

---
+++ src/VasriServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace ExoUNX\Vasri;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Service Provider for Laravel
+ * Class VasriServiceProvider
+ *
+ * @package ExoUNX\Vasri
+ * @author  Gaige Lama <gaigelama@gmail.com>
+ * @license MIT License
+ * @link    https://github.com/ExoUNX/Vasri
+ */
+class VasriServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->publishes(
+            [
+                __DIR__.'/../config/vasri.php' => config_path('vasri.php'),
+            ]
+        );
+    }
+
+}
